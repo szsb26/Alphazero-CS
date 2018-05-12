c16ecd4f9c5b02f6c822492adb6214a20ec00448
szhong26 <szhong26@gmail.com>
2018-05-12 00:11:17 -0400

    Add files via upload

--- alphazero_compressedsensing/compressed_sensing/CSGame.py
+++ alphazero_compressedsensing/compressed_sensing/CSGame.py
@@ -1,88 +1,71 @@
 from Game import Game
 from CSState import State
+import numpy as np
 
 class CSGame(Game):
-    def __init__(self, A, y): #A and y are both assumed to be numpy arrays
+    def __init__(self, A, y, k = 1): 
+    #A and y are both assumed to be numpy arrays/vectors
         self.sensing_matrix = A
         self.obs_vector = y
+        self.sparsity_limit = k
 
-    def getInitBoard(self): #Get the initial state at beginning of CS Game
-    	col_size = A.shape[1]
-    	Initial_State = CSState(np.ones(col_size))
+    def getInitBoard(self): 
+    #Get the initial state at beginning of CS Game
+    	col_size = self.sensing_matrix.shape[1]
+    	Initial_State = State(np.zeros(col_size + 1)) #The plus one is for the STOP action. If Initial_State.col[col_size] = 1, then algorithm
+    													#chooses to stop, and the next state is now a terminating state
+    	return Initial_State
 
-    def getActionSize(self): #return number of all actions (equal to column size of A)
-    	return A.shape[1]
+    def getActionSize(self): 
+    #return number of all actions (equal to column size of A + 1)
+    	return self.sensing_matrix.shape[1]+1 
 
-    def getNextState(self, state, action): #input is a CSState object and an action(integer). Output
-    											   #is the next state as a CSState object. 
-        if state.col[action] == 1:
-        	print ('Column already taken, invalid move')
+    def getNextState(self, state, action): 
+    #input is a State object and an action(integer). Output is the next state as a CSState object. 
+    	if state.col[-1] = 1:
+    		print ('Already at terminal state, no more actions can be taken.')
+    
+        elif state.col[action] == 1:
+        	print (str(action) + ' action already taken, invalid move.')
         	return
         else:
         	nextstate_col_ind = np.array(state.col)
         	nextstate_col_ind[action] = 1
-        	next_state = CSState(nextstate_col_ind)
+        	next_state = State(nextstate_col_ind)
         	return next_state 
 
-    def getValidMoves(self, state): #input is a CSState object. Output is a binary numpy vector for valid moves,
-    								#where b[i] = 1 implies ith column can be taken. 
+    def getValidMoves(self, state): 
+    #input is a State object. Output is a binary numpy vector for valid moves,
+    #where b[i] = 1 implies ith column can be taken. 
     
-        valid_moves = np.zeros(A.shape[1])
-        for i in range(A.shape[1]):
+        valid_moves = np.zeros(self.sensing_matrix.shape[1]+1)
+        for i in range(self.sensing_matrix.shape[1]+1):
         	if state.col[i] == 0:
         		valid_moves[i] = 1
 		
 		return valid_moves
 		
-    def getGameEnded(self, state): #check if state is a terminal state or not. If not terminal state, return 0. Otherwise,
-    							   #return (negative of?) the sparsity. 
-        """
-        Input:
-            board: current board
-
-        Returns:
-            r: 0 if game has not ended. 1 if player won, -1 if player lost,
-               small non-zero value for draw. 
-        """
-        pass
+    def getGameEnded(self, state): 
+    	#check if state is a terminal state or not. 
+    	#terminal state is decided by satisfying any one of the 3 conditions
+    	#	1)how many columns we have taken
+    	#	2)STOP action has been chosen state.col[-1] = 1
+    	#	3)whether Ax = y
+    	#   return reward of ||x||_0 + ||Ax - y||_2^2 where x is the solution to min_z||A_S*z - y||_2^2,
+    	#   where S is the set of columns chosen.
+    	#rewards at the terminal state should be negative because we are doing a minimization problem vs alphago maximization problem   
+    	
+    	A_S = np.matmul(self.sensing_matrix, state.col[0:state.col.size])
+    	x = np.linalg.lstsq(A_S, self.obs_vector)
+    	error = np.linalg.norm(np.matmul(A_S, x) - y)
+    	epsilon = 1e-10
+    	gamma = 1
         
-
-    def getCanonicalForm(self, board, player):
-        """
-        Input:
-            board: current board
-            player: current player (1 or -1)
-
-        Returns:
-            canonicalBoard: returns canonical form of board. The canonical form
-                            should be independent of player. For e.g. in chess,
-                            the canonical form can be chosen to be from the pov
-                            of white. When the player is white, we can return
-                            board as is. When the player is black, we can invert
-                            the colors and return the board.
-        """
-        pass
-
-    def getSymmetries(self, board, pi):
-        """
-        Input:
-            board: current board
-            pi: policy vector of size self.getActionSize()
-
-        Returns:
-            symmForms: a list of [(board,pi)] where each tuple is a symmetrical
-                       form of the board and the corresponding pi vector. This
-                       is used when training the neural network from examples.
-        """
-        pass
-
-    def stringRepresentation(self, board):
-        """
-        Input:
-            board: current board
-
-        Returns:
-            boardString: a quick conversion of board to a string format.
-                         Required by MCTS for hashing.
-        """
-        pass
+        if state.num_col = self.sparsity_limit or state.col[-1] = 1 or error<epsilon:
+        	reg = gamma*np.linalg.norm(np.matmul(A_S,x))
+        	r = - state.num_col - reg
+        	
+        return r
+        
+    def stringRepresentation(self, state):
+        return np.array_str(state.col)
--- alphazero_compressedsensing/compressed_sensing/CSState.py
+++ alphazero_compressedsensing/compressed_sensing/CSState.py
@@ -1,12 +1,13 @@
 from Game import Game
 import numpy as np
 
-#CSState is an object which stores information about the current state. 
+#CSState is an object which stores information about the current state. The input is a numpy array of 0 and 1's, where
+#the last index denotes whether the stopping action has been taken. self.col[i] = 0 means column has not yet been taken
+#while self.col[i] = 1 means column has already been taken. 
 
 class State():
     def __init__(self, columns_indicator):
-        self.col = columns_indicator #columns_indicator is a 0, 1 numpy vector where columns_indicator[i] = 0 
-        					 #means the ith column has not been taken while 1 means ith column has been taken.
-  		
+        self.col = columns_indicator 
+        self.num_col = np.sum(self.col[0:self.col.size])
   	      
   
