4e59f37d5385681aa1b896b66d6220f1095487ab
szhong26 <szhong26@gmail.com>
2018-05-12 16:59:21 -0400

    Add files via upload

--- alphazero_compressedsensing/compressed_sensing/CSGame.py
+++ alphazero_compressedsensing/compressed_sensing/CSGame.py
@@ -11,7 +11,7 @@ class CSGame(Game):
 
     def getInitBoard(self): 
     #Get the initial state at beginning of CS Game
-    	col_size = self.sensing_matrix.shape[1]
+    	col_size = self.sensing_matrix.shape[1] #column size
     	Initial_State = State(np.zeros(col_size + 1)) #The plus one is for the STOP action. If Initial_State.col[col_size] = 1, then algorithm
     													#chooses to stop, and the next state is now a terminating state
     	return Initial_State
@@ -22,13 +22,11 @@ class CSGame(Game):
 
     def getNextState(self, state, action): 
     #input is a State object and an action(integer). Output is the next state as a CSState object. 
-    	if state.col[-1] = 1:
-    		print ('Already at terminal state, no more actions can be taken.')
-    
-        elif state.col[action] == 1:
-        	print (str(action) + ' action already taken, invalid move.')
-        	return
-        else:
+    	if state.col[-1] == 1:
+    		print ('Already at terminal state, no more actions can be taken.')	
+    	elif state.col[action] == 1:
+    		print (str(action) + ' action already taken, invalid move.')
+    	else:
         	nextstate_col_ind = np.array(state.col)
         	nextstate_col_ind[action] = 1
         	next_state = State(nextstate_col_ind)
@@ -37,13 +35,11 @@ class CSGame(Game):
     def getValidMoves(self, state): 
     #input is a State object. Output is a binary numpy vector for valid moves,
     #where b[i] = 1 implies ith column can be taken. 
-    
-        valid_moves = np.zeros(self.sensing_matrix.shape[1]+1)
-        for i in range(self.sensing_matrix.shape[1]+1):
-        	if state.col[i] == 0:
-        		valid_moves[i] = 1
-		
-		return valid_moves
+    	valid_moves = np.zeros(self.sensing_matrix.shape[1]+1)
+    	for i in range(self.sensing_matrix.shape[1]+1):
+    		if state.col[i] == 0:
+    			valid_moves[i] = 1
+    	return valid_moves
 		
     def getGameEnded(self, state): 
     	#check if state is a terminal state or not. 
@@ -55,17 +51,19 @@ class CSGame(Game):
     	#   where S is the set of columns chosen.
     	#rewards at the terminal state should be negative because we are doing a minimization problem vs alphago maximization problem   
     	
-    	A_S = np.matmul(self.sensing_matrix, state.col[0:state.col.size])
+    	A_S = np.matmul(self.sensing_matrix, state.col[0:state.col.size]) 
     	x = np.linalg.lstsq(A_S, self.obs_vector)
     	error = np.linalg.norm(np.matmul(A_S, x) - y)
     	epsilon = 1e-10
     	gamma = 1
-        
-        if state.num_col = self.sparsity_limit or state.col[-1] = 1 or error<epsilon:
-        	reg = gamma*np.linalg.norm(np.matmul(A_S,x))
-        	r = - state.num_col - reg
-        	
-        return r
+    	
+    	if state.num_col == self.sparsity_limit or state.col[-1] == 1 or error<epsilon:
+    		reg = gamma*np.linalg.norm(np.matmul(A_S,x))
+    		r = - state.num_col - reg
+    		return r
+    	else:
+    		print("input state is not a terminal state")
+    		return 0
         
     def stringRepresentation(self, state):
         return np.array_str(state.col)
